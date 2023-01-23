d6cdf1e3e2352622b17b2129a67fc222ebad6ae7
jyky <jykymyrza@gmail.com>
2023-01-23 13:19:34 +0600

    start

---
+++ product/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from account.models import User
+
+# Create your models here.
+
+class Category(models.Model):
+    slug = models.SlugField()
+    title = models.CharField()
+
+
+class Products(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products')
+    title = models.CharField(max_length=30)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    image = models.ImageField(upload_to='products/', blank=False)
+    descriptions = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
